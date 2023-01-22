505e201584cfcf599acabedc88de2ce7f3800838
Tatevik Kalanchyan <tatev.kalanchyan@gamil.com>
2023-01-22 17:48:05 +0300

    declared List an added array filterin logic

--- project.cs
+++ project.cs
@@ -1,9 +1,16 @@
 using System;
-
+using System.Collections.Generic;
 public class HelloWorld
 {
     public static void Main(string[] args)
     {
         string[] array = {"hello", "2", "world", ":-)"};
+        List<string> list = new List<string>();
+
+        foreach (string item in array) {
+            if (item.Length <= 3) {
+                list.Add(item);
+            }
+        }
     }
 }
